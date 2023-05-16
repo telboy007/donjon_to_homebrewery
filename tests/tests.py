4f32ee81acc9928f111b9ed8fe4c56adbfbb9ca5
Terry Bonds <terry_bonds@yahoo.co.uk>
2023-05-16 17:58:45 +0100

    Add ability to customise look of certain parts of room locations

--- tests/tests.py
+++ tests/tests.py
@@ -1,25 +1,25 @@
 def test_fantasy():
     with open("./tests/fantasy.txt", "r", encoding="utf-8") as fantasy:
         check_value = len(fantasy.read())
-        assert check_value == 26585, f"Should be 26585 but is {check_value}"
+        assert check_value == 26227, f"Should be 26227 but is {check_value}"
 
 
 def test_adnd():
     with open("./tests/adnd.txt", "r", encoding="utf-8") as adnd:
         check_value = len(adnd.read())
-        assert check_value == 83410, f"Should be 83410 but is {check_value}"
+        assert check_value == 82437, f"Should be 82437 but is {check_value}"
 
 
 def test_4e():
     with open("./tests/4e.txt", "r", encoding="utf-8") as fourth:
         check_value = len(fourth.read())
-        assert check_value == 90057, f"Should be 90057 but is {check_value}"
+        assert check_value == 88857, f"Should be 88857 but is {check_value}"
 
 
 def test_5e():
     with open("./tests/5e.txt", "r", encoding="utf-8") as fifth:
         check_value = len(fifth.read())
-        assert check_value == 42160, f"Should be 42160 but is {check_value}"
+        assert check_value == 41766, f"Should be 41766 but is {check_value}"
 
 
 if __name__ == "__main__":
